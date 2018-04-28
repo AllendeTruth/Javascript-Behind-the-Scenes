66646515f070cc33c6490b3f1367a13452cc6907
AllendeTruth <nandersizzle@gmail.com>
2018-04-28 16:46:43 -0500

    hoisting and functions

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+calculateAge(1965);
 
 
+function calculateAge(year){
+    console.log(2018-year);
+}
+
+// hoisting - //function declarations //notice that you can call a funttion even before the function is written because functions are stored in the variable object immediately
+/calculateAge(1990);/ 
+retirement(1965);
+//hoisting- function expressions are not stored immediately like functions so you have to call them after. 
+var retirement = function(year){
+    console.log(65-(2018-year));
+}
+
+// retirement(1990);
+
+
+//////////VARIABLES////////
 
 
 
